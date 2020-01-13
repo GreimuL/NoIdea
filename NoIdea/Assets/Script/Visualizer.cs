e188ecf45ed8e0960dd2b10fed2d3cf08100282e
greimul <yoseib1998@naver.com>
2020-01-13 14:30:50 +0900

    update

--- NoIdea/Assets/Script/Visualizer.cs
+++ NoIdea/Assets/Script/Visualizer.cs
@@ -4,45 +4,47 @@ using UnityEngine;
 
 public class Visualizer : MonoBehaviour
 {
-    public const int BarCntSize = 64;
-    public const int SpectrumSize = 128;
     public AudioSource music;
-    public GameObject visual;
-    public Transform visP;
-    public RectTransform[] visuals = new RectTransform[BarCntSize];
-    public Light visualLight;
-    public float[] spectrum = new float[SpectrumSize];
+    float[] samples;
+
+    public Transform visualParent;
+    public GameObject visualBlock;
+    GameObject[] visualBlockArr;
+    public float scale;
+
     // Use this for initialization
     void Start()
     {
+        scale = 1000f;
         music = GameObject.Find("SampleMusic").GetComponent<AudioSource>();
-        float angle = 0.0f;
-        for (int i = 0; i < BarCntSize; i++)
+        samples = new float[512];
+        visualBlockArr = new GameObject[512];
+        float angle = 0f;
+        for(int i = 0; i < 512; i++)
         {
-            visuals[i] = Instantiate(visual).GetComponent<RectTransform>();
-            visuals[i].localPosition = new Vector2(3 * Mathf.Cos(Mathf.Deg2Rad * angle), 3 * Mathf.Sin(Mathf.Deg2Rad * angle));
-            visuals[i].localRotation = Quaternion.Euler(0, 0, angle + 90);
-            visuals[i].SetParent(visP);
-            visuals[i].sizeDelta = new Vector2(1, 1);
-            angle += 360.0f / (float)BarCntSize;
+            GameObject tempBlock = Instantiate(visualBlock);
+            RectTransform tempRect = tempBlock.GetComponent<RectTransform>();
+            tempRect.localPosition = new Vector2(3 * Mathf.Cos(Mathf.Deg2Rad * angle), 3 * Mathf.Sin(Mathf.Deg2Rad * angle));
+            tempRect.localRotation = Quaternion.Euler(0, 0, angle + 90);
+            tempRect.SetParent(visualParent);
+            tempRect.sizeDelta = new Vector2(1, 1);
+            angle += 360.0f / (float)512;
+            visualBlockArr[i] = tempBlock;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        music.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
-        visualLight.intensity = spectrum[20]*20f;
-        for (int i = 0; i < BarCntSize; i++)
-        {            
-            if (spectrum[i] * 5 > 0.5f)
-            {
-                visuals[i].sizeDelta = new Vector2(0.2f, 0.5f);
-            }
-            else
-            {
-                visuals[i].sizeDelta = new Vector2(0.2f, spectrum[i] * 20);
-            }
+        music.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+        for(int i = 0; i < 512; i++)
+        {
+            visualBlockArr[i].transform.localScale = new Vector3(5, samples[i]*scale, 5);
         }
+    }
+
+    void GetAmp()
+    {
+        
     }
 }
